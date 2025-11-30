41f7afd619221551928a084676630c032637cb37
hat0uma <55551571+hat0uma@users.noreply.github.com>
2025-11-30 21:04:36 +0900

    playform/inline

--- astro.config.ts
+++ astro.config.ts
@@ -11,6 +11,7 @@ import {
 import { transformerFileName } from "./src/utils/transformers/fileName";
 import { SITE } from "./src/config";
 import rehypeMermaidDualTheme from "./src/utils/rehype-mermaid-dual-theme";
+import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,6 +20,7 @@ export default defineConfig({
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
     }),
+    playformInline(),
   ],
   markdown: {
     syntaxHighlight: {
